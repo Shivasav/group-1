c67b69becf66a456de45c60334ea7d89e2b76df3
Shivasav <81324293+Shivasav@users.noreply.github.com>
2024-12-09 22:38:12 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const corsOptions = {
 app.use(cors(corsOptions)); 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
-
+app.options('*', cors(corsOptions));
 //error handel
 app.use((err, req, res, next) => {
   const status = err.status || 500;
